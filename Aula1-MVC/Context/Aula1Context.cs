7004ed73801515b5e726939bf4e445c89162d702
luizhsalazar <luizhsalazar@gmail.com>
2017-05-09 17:24:55 -0300

    Scaffold to build simple controller and views structure

---
+++ Aula1-MVC/Context/Aula1Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using Aula1_MVC.Models;
+
+namespace Aula1_MVC.Context
+{
+    public class Aula1Context : DbContext
+    {
+        public Aula1Context()
+            : base("Aula1Context")
+        {
+
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
